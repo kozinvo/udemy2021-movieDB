6500c7927fd1fb92d5f2a748aa7db5b807ba1d5e
kozinvo <kozinvo333@gmail.com>
2021-12-02 01:50:40 +0500

    lesson15

--- script.js
+++ script.js
@@ -11,12 +11,30 @@ const personalMovieDB = {
     privat: false,
 };
 
-const lastFilm = prompt('Один из последних просмотренных фильмов', ''),
-    filmRating = prompt('На сколько оцениет его?', ''),
-    lastFilm2 = prompt('Один из последних просмотренных фильмов', ''),
-    filmRating2 = prompt('На сколько оцениет его?', '');
+if (personalMovieDB.count < 10 && personalMovieDB.count > 0) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log("Произошла ошибка");
+}
 
-personalMovieDB.movies[lastFilm] = filmRating;
-personalMovieDB.movies[lastFilm2] = filmRating2;
+
+for (let i = 1; i <= 2; i++) {
+    const lastFilm = prompt('Один из последних просмотренных фильмов', ''),
+        filmRating = prompt('На сколько оцениет его?', '');
+
+    if (lastFilm != null && filmRating != null && lastFilm != '' && filmRating != '' && lastFilm.length < 50) {
+        personalMovieDB.movies[lastFilm] = filmRating;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+
 
 console.log(personalMovieDB);
